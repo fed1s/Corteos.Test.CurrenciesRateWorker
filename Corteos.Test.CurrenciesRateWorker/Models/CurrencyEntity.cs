53f60f7438d2ed0a60c221121169a4335663a6fe
Fedor.Lyapin <fedis2105@gmail.com>
2024-08-06 09:06:50 +0300

    Добавлены комментарии к наименованиям свойств моделей БД.

--- Corteos.Test.CurrenciesRateWorker/Models/CurrencyEntity.cs
+++ Corteos.Test.CurrenciesRateWorker/Models/CurrencyEntity.cs
@@ -2,12 +2,21 @@ namespace Corteos.Test.CurrenciesRateWorker.Models
 {
     public class CurrencyEntity
     {
+        /// <summary>
+        /// Международный числовой код валюты.
+        /// </summary>
         public int ISONumCodeId { get; set; } = 0;
-
+        /// <summary>
+        /// Международный символьный код валюты.
+        /// </summary>
         public string ISOCharCode { get; set; } = string.Empty;
-
+        /// <summary>
+        /// Наименование валюты.
+        /// </summary>
         public string CurrencyName { get; set; } = string.Empty;
-
+        /// <summary>
+        /// Наименование валюты (английское).
+        /// </summary>
         public string CurrencyEngName { get; set; } = string.Empty;
 
         public ICollection<CurrencyRateEntity>? CurrenciesRate { get; set; }
--- Corteos.Test.CurrenciesRateWorker/Models/CurrencyRateEntity.cs
+++ Corteos.Test.CurrenciesRateWorker/Models/CurrencyRateEntity.cs
@@ -2,12 +2,21 @@ namespace Corteos.Test.CurrenciesRateWorker.Models
 {
     public class CurrencyRateEntity
     {
+        /// <summary>
+        /// Дата курса валюты.
+        /// </summary>
         public DateOnly CurrencyRateDate { get; set; } = new DateOnly();
-
+        /// <summary>
+        /// Номинал обменного курса.
+        /// </summary>
         public int Nominal { get; set; } = 0;
-
+        /// <summary>
+        /// Значение обменного курса.
+        /// </summary>
         public decimal Value { get; set; } = 0;
-
+        /// <summary>
+        /// Международный числовой код валюты.
+        /// </summary>
         public int NumCodeId { get; set; } = 0;
 
         public CurrencyEntity? Currency { get; set; }
